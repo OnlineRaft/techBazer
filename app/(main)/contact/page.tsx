b6e074868f01f80bd96f12fd5b8a2969822143cb
OnlineRaft <apps@onlineraft.com>
2025-07-18 09:23:19 +0200

    new contact

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -1,12 +1,13 @@
 import ContactPageOne from '@/components/pages/contact-pages/ContactPageOne'
 import ContactPageTwo from '@/components/pages/contact-pages/ContactPageTwo'
+import ContactPageTwo from '@/components/pages/contact-pages/ContactPageThree'
 import React from 'react'
 
 const ContactPage = () => {
   return (
     <div>
       {/* <ContactPageOne /> */}
-      <ContactPageTwo />
+      <ContactPageThree />
     </div>
   )
 }
